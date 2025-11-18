6fa2447642a19c67b99ae98e3f591973f687f19d
Tomas Gomez Cifuentes <tomicifuentesposada@gmail.com>
2025-11-17 21:24:47 -0500

    intento 3.0

--- FrontendVictus/src/config/api.js
+++ FrontendVictus/src/config/api.js
@@ -1,7 +1,7 @@
 export const API = {
   base: import.meta.env.VITE_API_URL,
   // Prefijo real para CRUD del backend
-  v1: `${import.meta.env.VITE_API_URL}/uco-challenge/api/v1`,
+  v1: `${import.meta.env.VITE_API_URL}/uco/api/v1`,
   // Prefijo real para streams (SSE)
   streamV1: `${import.meta.env.VITE_API_URL}/api/v1`,
 };
